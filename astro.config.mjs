4162f33c12a8f5287703a2cbb7cdc9e5d375498c
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-01-25 11:00:31 +0100

    Update to use Astro 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(
+		{
+			extendMarkdownConfig: false,
+			smartypants: true,
+			gfm: true,
+		}
+	), sitemap()],
 });
